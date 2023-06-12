404f1ddd60040d404e91f94c103529e03a7e6fa0
ND Morsalin <morsalin056@gmail.com>
2023-06-12 10:47:24 +0600

    sort payment result

--- controller/StudentController/enrolledClass.js
+++ controller/StudentController/enrolledClass.js
@@ -2,7 +2,7 @@ const { EnrolledClasses } = require("../../Models/Students/enrolledClasses")
 
 const getEnrolledClasses = async (req, res, next) => {
     const studentid = req.headers?.studentid
-    const enrolledClasses = await EnrolledClasses.find({ studentId: studentid }).toArray()
+    const enrolledClasses = await EnrolledClasses.find({ studentId: studentid }).sort({ paymentDate: -1 }).toArray()
     console.log(enrolledClasses);
     res.json(enrolledClasses)
 
